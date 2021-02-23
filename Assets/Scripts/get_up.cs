0b1c732dd5d6141153ce2e587add71221e0f00b9
yochaym22 <yochaym22@gmail.com>
2021-02-23 23:43:10 +0200

    stand up

---
+++ Assets/Scripts/get_up.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class get_up : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator clock_anim;
+    public Animator player_anim;
+    public Transform transform; 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!(clock_anim.GetBool("is_ringing")))
+        {
+            player_anim.SetBool("getting_up", true);
+            
+        }
+        //Debug.Log(transform.position.x.ToString());
+
+
+
+    }
+}
