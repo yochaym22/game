89c59c47043f83298ec4615d38515d5aac08db40
mayao <mayaoffir27@gmail.com>
2021-02-23 15:04:33 +0200

    walking animation

--- Assets/bedroom/alarm_clock.cs
+++ Assets/bedroom/alarm_clock.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class alarm_clock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void clockVisibilty ()
     {
-        
+      Renderer rend = gameObject.GetComponent<Renderer>();
+      if (rend.enabled)
+        rend.enabled = false;
+      else
+        rend.enabled = true;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
