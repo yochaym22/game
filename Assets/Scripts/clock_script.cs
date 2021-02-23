a0f78a33673e2d714560d0089dadde006b10209d
yochaym22 <yochaym22@gmail.com>
2021-02-23 15:33:00 +0200

    alarm clock functionallity

---
+++ Assets/Scripts/clock_script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clock_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+
+
+    void Start()
+    {
+     
+       animator.SetBool("is_ringing", true);
+      
+
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("is_ringing", false);
+        }
+    }
+}
