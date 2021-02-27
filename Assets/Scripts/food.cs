ad0456f7dcfc153b63b7d33c97100d6f5d201a80
mayao <mayaoffir27@gmail.com>
2021-02-28 01:17:01 +0200

    living room

---
+++ Assets/Scripts/food.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class food : MonoBehaviour
+{
+
+    Animator anim;
+    void OnMouseDown()
+    {
+        anim.SetTrigger("Active_food");
+
+    }
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+}
