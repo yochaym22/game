3607bb59ad73593d226155d757043c744a348352
mayao <mayaoffir27@gmail.com>
2021-02-28 00:19:44 +0200

    bus

---
+++ Assets/Scripts/bus_moving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bus_moving : MonoBehaviour
+{
+
+    Animator anim;
+    public GameObject player;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Active");
+            player.active = false;
+        }
+    }
+}
