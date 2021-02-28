8a56b69a3d2e084af395759d3524107816b27fb5
yochaym22 <yochaym22@gmail.com>
2021-02-28 14:44:23 +0200

    getting brush

---
+++ Assets/Scripts/green_brushing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class green_brushing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator playAnimator;
+    public GameObject playerHand;
+    public Vector3 brush_positon;
+    public Vector3 brush_roation;
+    Transform brush;
+
+    void Start()
+    {
+         brush = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Transform parent = brush.parent;
+        brush.transform.parent = playerHand.transform;
+        brush.localPosition = brush_positon;
+        brush.localEulerAngles = brush_roation;
+        playAnimator.SetBool("is_brushing", true);
+
+
+    }
+}
