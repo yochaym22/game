75124d036a9ac82367471de0237b35f457056bf4
yochaym22 <yochaym22@gmail.com>
2021-02-28 13:28:14 +0200

    brush teeth

--- Assets/Scripts/secne_loader.cs
+++ Assets/Scripts/secne_loader.cs
@@ -6,14 +6,35 @@ using UnityEngine.SceneManagement;
 
 public class secne_loader : MonoBehaviour
 {
-    [SerializeField] string nextLevelName;   
+    [SerializeField] string nextLevelName;
+    BoxCollider2D collider2D;
+    SpriteRenderer sprite;
+    public Scene local_scene;
+    public Animator playerAnimator;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        collider2D = GetComponent<BoxCollider2D>();
+    }
     // Update is called once per frame
     void Update()
     {
-
+        if(local_scene.name == "bedroom")
+        {
+            if(playerAnimator.GetBool("get_up_done"))
+            {
+                sprite.enabled = true;
+                collider2D.enabled = true;
+            }
+        }
+        if (local_scene.name == "bathroom")
     }
     private void OnMouseDown()
     {
-        SceneManager.LoadScene(nextLevelName);
+        if (sprite.enabled == true && collider2D.enabled ==true  )
+        {
+            SceneManager.LoadScene(nextLevelName);
+        }
     }
 }
