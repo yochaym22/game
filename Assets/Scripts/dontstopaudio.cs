594404fb739015107350076a03e602eba70d8b7d
mayao <mayaoffir27@gmail.com>
2021-02-28 19:04:14 +0200

    music

---
+++ Assets/Scripts/dontstopaudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontstopaudio : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+}
